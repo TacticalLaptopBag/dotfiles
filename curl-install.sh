936f971baceb1a2fb171290a423017fd9c68410c
Tactical Laptop Bag <137741188+TacticalLaptopBag@users.noreply.github.com>
2024-08-14 15:50:37 -0400

    Update curl-install.sh

--- curl-install.sh
+++ curl-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
-git clone git@github.com:TacticalLaptopBag/dotfiles.git ~/.files/ || git clone https://github.com/TacticalLaptopBag/dotfiles.git
-~/.files/setup.sh
+OLD_CWD=`pwd`
 
+git clone git@github.com:TacticalLaptopBag/dotfiles.git ~/.files/ || git clone https://github.com/TacticalLaptopBag/dotfiles.git ~/.files/
+cd ~/.files/
+./setup.sh
+
+cd "$OLD_CWD"
