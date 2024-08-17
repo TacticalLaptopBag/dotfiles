9fb26ef482fe69e0ec148a40b68602acd31e352b
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2024-08-16 20:32:39 -0400

    curl install now populates submodules too

--- curl-install.sh
+++ curl-install.sh
@@ -4,6 +4,7 @@ OLD_CWD=`pwd`
 
 git clone git@github.com:TacticalLaptopBag/dotfiles.git ~/.files/ || git clone https://github.com/TacticalLaptopBag/dotfiles.git ~/.files/
 cd ~/.files/
+git submodule update --init --recursive
 ./setup.sh
 
 cd "$OLD_CWD"
