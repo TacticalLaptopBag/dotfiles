1d63e83849013a144f972f59dfff7e9baebfd1a8
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2024-08-16 20:27:21 -0400

    Autoinstall dependencies

--- setup.sh
+++ setup.sh
@@ -8,8 +8,28 @@ verify_command() {
   fi
 }
 
+try_install_depends() {
+  which apt-get > /dev/null
+  if [ "$?" == "0" ]; then
+    echo "apt detected, installing dependencies..."
+    which node > /dev/null
+
+    # Sometimes, Node isn't installed through apt. If it isn't installed at all, it's probably safe to use apt.
+    NODE_DEPENDS=""
+    if [ "$?" != "0" ]; then
+      NODE_DEPENDS="node npm"
+    fi
+    sudo apt-get update -y
+    sudo apt-get install python3 python3-venv git stow $NODE_DEPENDS
+  fi
+}
+
+try_install_depends
+
 verify_command git
 verify_command stow
+verify_command npm
+verify_command node
 set -e
 
 echo "Replacing dotfiles..."
