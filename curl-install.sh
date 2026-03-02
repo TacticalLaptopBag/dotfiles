9106836155ead84ba0e652f0cc9bcbd91df40dff
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2026-03-02 16:34:53 -0500

    Fix read returning non-zero

--- curl-install.sh
+++ curl-install.sh
@@ -22,9 +22,7 @@ git submodule update --init --recursive
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 while true; do
-    unset -e
-    read -rp "Would you like to run system.sh or user.sh? (system/user/both/n): " run_init
-    set -e
+    read -rp "Would you like to run system.sh or user.sh? (system/user/both/n): " run_init < /dev/tty
     case "$run_init" in
         system) SKIP_USER_SH=1 sudo "$HOME/.files/init/system.sh"; break ;;
         user) SKIP_SYSTEM_SH=1 "$HOME/.files/init/user.sh"; break ;;
